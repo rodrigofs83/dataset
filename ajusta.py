1ed17f8548c364764318805f16b88285e4da2db5
rodrigofs83 <rodrigofranciscodasilva83@gmail.com>
2024-09-04 16:03:21 -0300

    atualização2

--- ajusta.py
+++ ajusta.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Passo 1: Ler o arquivo CSV
 df = pd.read_csv('data.csv')
@@ -8,15 +9,25 @@ print("DataFrame original:")
 print(df)
 
 # Passo 2: Trocar as colunas de índice 0 e 10
-colunas = df.columns.tolist()  # Converter as colunas em uma lista
-colunas[0], colunas[1] = colunas[1], colunas[0]  # Trocar as colunas
-
+#colunas = df.columns.tolist()  # Converter as colunas em uma lista
+#colunas[0], colunas[1] = colunas[1], colunas[0]  # Trocar as colunas
+datasets = pd.read_csv('data.csv',header=None, skiprows=1)
+# Passo 2: Trocar as colunas de índice 0 e 10
+#colunas = datasets.columns.tolist()  # Converter as colunas em uma lista
+#colunas[1], colunas[0] = colunas[1], colunas[0]  # Trocar as colunas
+label_encoder = LabelEncoder()
+colunas[0] = label_encoder.fit_transform(colunas[0])
 # Reorganizar o dataframe com as colunas trocadas
-df = df[colunas]
+#datasets= datasets[colunas]
+# Remover espaços em branco de todo o DataFrame (se houver)
+#datasets = datasets.dropna(axis=1, how='any')
+# Reorganizar o dataframe com as colunas trocadas
+df = datasets 
 
 # Exibir o dataframe após a troca
 print("\nDataFrame após a troca das colunas 0 e 10:")
-print(df)
 
+print(datasets)
+#print(datasets)
 # Passo 3: (Opcional) Salvar o dataframe modificado em um novo arquivo CSV
 df.to_csv('data1.csv', index=False)
